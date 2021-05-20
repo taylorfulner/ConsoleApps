cf9ef695d03d3430c519d848f2074bf670734de1
Taylor <taylorfulner@gmail.com>
2021-05-19 22:40:54 -0400

    badges repo created

--- Badges_Repository/Badges.cs
+++ Badges_Repository/Badges.cs
@@ -11,5 +11,14 @@ namespace Badges_Repository
         public int BadgeID { get; set; }
 
         public List<string> DoorName { get; set; } = new List<string>() { "A1", "A2", "A3", "A4", "A5", "A6", "B1", "B2", "B3", "B4", "B5", "C1", "C2" };
+
+        public Badges() { }
+
+        public Badges(int badgeID, List<string> doorName)
+        {
+            BadgeID = badgeID;
+            DoorName = doorName;
+        }
+
     }
 }
--- Badges_Repository/BadgesRepository.cs
+++ Badges_Repository/BadgesRepository.cs
@@ -8,6 +8,48 @@ namespace Badges_Repository
 {
     public class BadgesRepository
     {
+        public Dictionary<int, List<string>> badgeDictionary = new Dictionary<int, List<string>>();
+
+        public void SeedList()
+        {
+            badgeDictionary.Add(123, new List<string> { "A2", "A4" });
+        }
         
+        public bool CreateNewBadge(int newBadgeID, List<string> door)
+        {
+            int startingCount = badgeDictionary.Count;
+            badgeDictionary.Add(newBadgeID, door);
+            bool wasAdded = (badgeDictionary.Count > startingCount) ? true : false;
+            return wasAdded;
+        }
+
+        public Dictionary<int, List<string>> SeeAllBadges()
+        {
+            return badgeDictionary;
+        }
+
+        public Badges GetBadgeAccess(int id) //FIX TO dictionary type?
+        {
+            foreach (KeyValuePair<int, List<string>> badge in badgeDictionary)
+            {
+                Console.WriteLine($"Key = {badge.Key}, Value = {badge.Value}");
+            }
+            return null;
+        }
+
+        public bool UpdateBadgeInfo(int id, Badges newBadgeInfo)
+        {
+            Badges oldBadgeInfo = GetBadgeAccess(id);
+            if (oldBadgeInfo != null)
+            {
+                oldBadgeInfo.BadgeID = newBadgeInfo.BadgeID;
+                oldBadgeInfo.DoorName = newBadgeInfo.DoorName;
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
     }
 }
